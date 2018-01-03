4a8880968799767543ff66862a665f015a50a0bb
copperkraft <copperkraft@gmail.com>
2018-01-03 13:06:09 +0300

    Moved authorisation logic to service

--- src/data/repositories/interfaces/user.repository.ts
+++ src/data/repositories/interfaces/user.repository.ts
@@ -2,6 +2,5 @@ import { BaseRepository } from './base.repository';
 import { UserAttribute } from '../../models/user';
 
 export interface UserRepository extends BaseRepository<UserAttribute> {
-    authorize(credentials: {email: string, password: string}): Promise<UserAttribute>;
-    register(userData: {email: string, password: string, name: string}): Promise<UserAttribute>;
+
 }
--- src/logic/services/user.service.ts
+++ src/logic/services/user.service.ts
@@ -1,16 +1,42 @@
 import { UserRepository } from '../../data/repositories/interfaces/user.repository';
 import { inject, injectable } from 'inversify';
 import { UserAttribute } from '../../data/models/user';
+import { Encryption } from '../../helpers/encryption-helper';
+import { User } from '../models/user';
 
 @injectable()
 export class UserService {
     constructor(@inject('UserRepository') private userRepository: UserRepository) { }
     
     async authorize(credentials: {email: string, password: string}): Promise<UserAttribute> {
-        return this.userRepository.authorize(credentials);
+        const user = await this.userRepository.findOne({
+            where: {
+                email: credentials.email
+            }
+        });
+        if (user) {
+            if (Encryption.check(credentials.password, user.salt, user.password)) {
+                return new User(user);
+            } else {
+                throw new Error('wrong password');
+            }
+        } else {
+            throw new Error('wrong email');
+        }
     }
     
     async register(userData: {email: string, password: string, name: string}): Promise<UserAttribute> {
-        return this.userRepository.register(userData);
+        const salt = Encryption.generateSalt().toString();
+        const user = await this.userRepository.create({
+            email: userData.email,
+            name: userData.name,
+            salt,
+            password: Encryption.encrypt(userData.password, salt)
+        });
+        if (user) {
+            return new User(user);
+        } else {
+            throw new Error('unable to create user');
+        }
     }
 }
